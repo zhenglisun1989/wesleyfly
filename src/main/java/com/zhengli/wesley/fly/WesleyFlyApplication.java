80d9378ef656070d591cfe6fa065f55634586fd7
zhengli <zhenglisun1989@163.com>
2020-06-04 23:19:26 +0800

    init wesley fly

---
+++ src/main/java/com/zhengli/wesley/fly/WesleyFlyApplication.java
@@ -0,0 +1,13 @@
+package com.zhengli.wesley.fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesleyFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesleyFlyApplication.class, args);
+	}
+
+}
