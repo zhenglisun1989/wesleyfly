856a2ad9b4f4612646fa46c59b41bc668419a474
zhenglisun1989 <zhenglisun1989@163.com>
2020-06-15 08:21:16 +0800

    init wesley fly

---
+++ src/main/java/com/zhengli/wesley/fly/util/NexusUtil.java
@@ -0,0 +1,17 @@
+package com.zhengli.wesley.fly.util;
+
+/**
+ * Created by sunzhengli on 2020/6/14
+ */
+public class NexusUtil {
+
+    public void sayHello1() {
+        System.out.println("good man wesley");
+    }
+    public void sayHello2() {
+        System.out.println("good man wesley");
+    }
+    public void sayHello3() {
+        System.out.println("good man wesley");
+    }
+}
